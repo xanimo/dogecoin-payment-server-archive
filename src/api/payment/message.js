362d4033dc6f349b41518ca8b3f29d0b191c68fe
rllola <rllola80@gmail.com>
2021-12-29 13:34:58 +0100

    forgot a file

--- src/api/payment/message.js
+++ src/api/payment/message.js
@@ -1,4 +1,4 @@
-const { MissingFieldError } = require('./error')
+const { MissingFieldError } = require('../error')
 
 /*
   Message received when we want to create a micro payment buying an item identified by `ref`
