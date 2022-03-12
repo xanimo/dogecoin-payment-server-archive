c7c344fbf868fe462c541ccb0961dbb92762e543
rllola <rllola80@gmail.com>
2022-03-12 17:35:40 +0100

    successgit add --all .

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,6 @@ const payment = require('./payment')
 const pubkey = require('./pubkey')
 const notification = require('./notification')
 const close = require('./close')
-const logger = require('#logging')
 
 const router = express.Router()
 
@@ -20,7 +19,6 @@ router.use('/close', close)
 // Error handler
 router.use(function (err, req, res, next) {
   console.log(err)
-  logger.error(err)
   res.status(err.status || 500)
   res.json({
     message: err.message,
--- src/paymentchannel/util.js
+++ src/paymentchannel/util.js
@@ -1,14 +1,12 @@
 const { sign, decodeTx, prepareTransactionToSign, encodeRawTransaction } = require('../utils/tx')
 const { doubleHash } = require('../utils/hash')
 const CompactSize = require('../utils/compactSize')
-const { createPayToHash } = require('../utils/address')
 
 function signPaymentChannelTx (rawtx, payerSignature, redeemScript, privkey) {
   let tx = decodeTx(rawtx)
 
   // Probably not redeem script but p2sh
-  tx.txIns[0].signature = createPayToHash(redeemScript).script
-  console.log(tx)
+  tx.txIns[0].signature = redeemScript
 
   tx.hashCodeType = 1
   const rawTx = prepareTransactionToSign(tx, 0)
@@ -18,28 +16,27 @@ function signPaymentChannelTx (rawtx, payerSignature, redeemScript, privkey) {
   delete tx.hashCodeType
 
   const sizeRedeemScript = CompactSize.fromSize(redeemScript.length)
-  const sizeSigPayee = CompactSize.fromSize(payerSignature.length + 1)
-  const sizeSigPayer = CompactSize.fromSize(signature.length + 1)
+  const sizeSigPayer = CompactSize.fromSize(payerSignature.length + 1)
+  const sizeSigPayee = CompactSize.fromSize(signature.length + 1)
 
   // need to build sig script
   // Do we need to know the order of the pubkey ?
   const sigScript = Buffer.from('00' +
     sizeSigPayee.toString('hex') +
-    payerSignature.toString('hex') +
+    signature.toString('hex') +
     '01' +
     sizeSigPayer.toString('hex') +
-    signature.toString('hex') +
+    payerSignature.toString('hex') +
     '01' +
     '00' +
     '4c' +
     sizeRedeemScript.toString('hex') +
-    redeemScript.toStringt('hex'),
+    redeemScript.toString('hex'),
   'hex')
 
   tx.txIns[0].signature = sigScript
   tx.txIns[0].signatureSize = CompactSize.fromSize(sigScript.length)
 
-  console.log(sigScript.toString('hex'))
   tx = encodeRawTransaction(tx)
 
   return tx
